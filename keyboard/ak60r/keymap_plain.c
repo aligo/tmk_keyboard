8778da78a5ce6bb9a1d86af190a696f81ef15138
aligo Kang <aligo_x@163.com>
2015-08-31 13:53:55 +0800

    ak60r plain

--- keyboard/ak60r/keymap_plain.c
+++ keyboard/ak60r/keymap_plain.c
@@ -2,9 +2,9 @@
 
 const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* 0: qwerty */
-    KEYMAP_AK60( GRV, 1,   2,   3,   4,   5,   6,   7,   8,   9,   0,   MINS,EQL, BSLS,ESC,  \
+    KEYMAP_AK60( ESC, 1,   2,   3,   4,   5,   6,   7,   8,   9,   0,   MINS,EQL, BSLS,GRV,  \
                  TAB, Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   LBRC,RBRC,BSPC,      \
-                 LCTL,A,   S,   D,   F,   G,   H,   J,   K,   L,   SCLN,QUOT,     ENT,       \
+                 CAPS,A,   S,   D,   F,   G,   H,   J,   K,   L,   SCLN,QUOT,     ENT,       \
                  LSFT,NO,  Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, SLSH,RSFT,NO,        \
                  LCTL,LALT,LGUI,          SPC,                     NO,  RGUI,RALT,NO)
 };
